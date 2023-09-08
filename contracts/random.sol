f15b4927df1c60e9bfd5b1265ab53c6ac4505ff6
asdf215 <jubeatxreflecbeat@gmail.com>
2023-09-08 11:00:54 +0900

    third commit

--- contracts/random.sol
+++ contracts/random.sol
@@ -1,7 +1,3 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 < 0.9.0;
 
-contract Random {
-    event paidAddress(address indexed sender, uint256 payment);
-    event winnerAddress(address indexed winner);
-}
